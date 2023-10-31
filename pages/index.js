412b4f2b701c36b091bf89b65ae3bc5affc93197
Hasan0734 <jahidhasan2856@gmail.com>
2023-10-31 23:14:20 +0600

    added server api

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,59 @@
-import React from 'react'
-export default function Home() {
+import React, { useEffect, useState } from "react";
+import clientPromise from "../lib/mongodb";
+
+const labels = ["January", "February", "March", "April", "May", "June"];
+
+export default function Home({ isConnected }) {
+  const [data, setData] = useState([
+    { _id: "trey", DATE: ["2023-10-19"], PRICE: ["100"] },
+  ]);
+
+  data[1]["PRICE"];
+  data[1]["DATE"];
+
+  useEffect(() => {
+    (async () => {
+      const results = await fetch("api/list");
+      const resultsJson = await results.json();
+      setData(resultsJson);
+    })();
+  }, []);
+
+  let kim = data[0]["DATE"];
+  let him = data[0]["PRICE"];
+  const pata = {
+    labels: kim,
+    datasets: [
+      {
+        label:
+          "Producer Price Index by Industry: Deep Sea Freight Transportation",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(0,0,255)",
+        data: him,
+      },
+    ],
+  };
+
   return (
     <div>
       <h2>Hello world</h2>
     </div>
-  )
+  );
 }
 
-
-
+export async function getServerSideProps(context) {
+  try {
+    await clientPromise;
+    return {
+      props: { isConnected: true },
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      props: { isConnected: false },
+    };
+  }
+}
 
 // ./components/LineChart.js
 
@@ -18,17 +63,13 @@ export default function Home() {
 // import { useState, useEffect } from "react";
 // import clientPromise from '../lib/mongodb';
 
-
-
 // const labels = ["January", "February", "March", "April", "May", "June"];
 
 // const LineChart = ({isConnected}) => {
-    
+
 //     // data[1]['PRICE']
 //     // data[1]['DATE']
 
-
-    
 // const [data, setData] = useState([{_id:'trey', DATE:['2023-10-19'], PRICE:['100']}]);
 
 //   // useEffect(() => {
@@ -52,15 +93,12 @@ export default function Home() {
 //         data: him,
 //       },
 //     ],
-    
+
 //   };
-
-
-
 
 //   return (
 //     <div className="h-96">
-//       {/* <Line data={pata} 
+//       {/* <Line data={pata}
 //                     /> */}
 //       <h2>Hello world!</h2>
 //     </div>
@@ -81,6 +119,5 @@ export default function Home() {
 // //       props : {isConnected: false}
 // //     }
 // //   }
-
 
 // // }
