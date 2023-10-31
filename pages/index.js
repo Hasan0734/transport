b9944ed7bf23aac432206cfb2c929580cdb4516b
Hasan0734 <jahidhasan2856@gmail.com>
2023-10-31 23:09:20 +0600

    starting from new page

--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,86 @@
+import React from 'react'
+export default function Home() {
+  return (
+    <div>
+      <h2>Hello world</h2>
+    </div>
+  )
+}
+
+
+
+
 // ./components/LineChart.js
 
-import React from "react";
-import Chart from 'chart.js/auto'
-import { Line } from "react-chartjs-2";
-import { useState, useEffect } from "react";
-import clientPromise from '../lib/mongodb';
+// import React from "react";
+// import Chart from 'chart.js/auto'
+// import { Line } from "react-chartjs-2";
+// import { useState, useEffect } from "react";
+// import clientPromise from '../lib/mongodb';
 
 
 
-const labels = ["January", "February", "March", "April", "May", "June"];
+// const labels = ["January", "February", "March", "April", "May", "June"];
 
-const LineChart = ({isConnected}) => {
+// const LineChart = ({isConnected}) => {
     
-    // data[1]['PRICE']
-    // data[1]['DATE']
+//     // data[1]['PRICE']
+//     // data[1]['DATE']
 
 
     
-const [data, setData] = useState([{_id:'trey', DATE:['2023-10-19'], PRICE:['100']}]);
+// const [data, setData] = useState([{_id:'trey', DATE:['2023-10-19'], PRICE:['100']}]);
 
-  // useEffect(() => {
-  //   (async () => {
-  //     const results = await fetch("api/list");
-  //     const resultsJson = await results.json();
-  //     setData(resultsJson);
-  //   })();
-  // }, []);
+//   // useEffect(() => {
+//   //   (async () => {
+//   //     const results = await fetch("api/list");
+//   //     const resultsJson = await results.json();
+//   //     setData(resultsJson);
+//   //   })();
+//   // }, []);
 
-let kim = data[0]['DATE']
-let him = data[0]['PRICE']
+// let kim = data[0]['DATE']
+// let him = data[0]['PRICE']
 
-  const pata = {
-    labels: kim,
-    datasets: [
-      {
-        label: "Producer Price Index by Industry: Deep Sea Freight Transportation",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(0,0,255)",
-        data: him,
-      },
-    ],
+//   const pata = {
+//     labels: kim,
+//     datasets: [
+//       {
+//         label: "Producer Price Index by Industry: Deep Sea Freight Transportation",
+//         backgroundColor: "rgb(255, 99, 132)",
+//         borderColor: "rgb(0,0,255)",
+//         data: him,
+//       },
+//     ],
     
-  };
+//   };
 
 
 
 
-  return (
-    <div className="h-96">
-      {/* <Line data={pata} 
-                    /> */}
-      <h2>Hello world!</h2>
-    </div>
-  );
-};
+//   return (
+//     <div className="h-96">
+//       {/* <Line data={pata} 
+//                     /> */}
+//       <h2>Hello world!</h2>
+//     </div>
+//   );
+// };
 
-export default LineChart;
+// export default LineChart;
 
-// export async function getServerSideProps(context){
-//   try{
-//     await clientPromise
-//     return {
-//       props : {isConnected:true}
-//     }
-//   } catch (e){
-//     console.error(e)
-//     return{
-//       props : {isConnected: false}
-//     }
-//   }
+// // export async function getServerSideProps(context){
+// //   try{
+// //     await clientPromise
+// //     return {
+// //       props : {isConnected:true}
+// //     }
+// //   } catch (e){
+// //     console.error(e)
+// //     return{
+// //       props : {isConnected: false}
+// //     }
+// //   }
 
 
-// }
+// // }
